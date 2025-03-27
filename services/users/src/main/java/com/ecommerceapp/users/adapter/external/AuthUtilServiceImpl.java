4e1e85eb4cd98dd1bee4dcea05e0d043cb12a509
baothaihcmut <thaibao22042k4@gmail.com>
2025-03-27 14:50:06 +0700

    users/done token

--- services/users/src/main/java/com/ecommerceapp/users/adapter/external/AuthUtilServiceImpl.java
+++ services/users/src/main/java/com/ecommerceapp/users/adapter/external/AuthUtilServiceImpl.java
@@ -1,8 +1,8 @@
 package com.ecommerceapp.users.adapter.external;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
+import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Service;
 
 import com.ecommerceapp.users.core.port.outbound.external.AuthUtilService;
 import com.ecommerceapp.users.core.port.outbound.external.models.AccessTokenPayload;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSObject;
-import com.nimbusds.jose.Payload;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jwt.JWTClaimsSet;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,58 +24,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthUtilServiceImpl implements AuthUtilService{
     private final JwtProperties jwtProperties;
-
+    private final ObjectMapper objectMapper;
+    private Key accessTokenKey;
+    private Key refreshTokenKey;
+    @PostConstruct
+    public void init() {
+        this.accessTokenKey = Keys.hmacShaKeyFor(this.jwtProperties.getAccessToken().getSecret().getBytes());
+        this.refreshTokenKey = Keys.hmacShaKeyFor(this.jwtProperties.getRefreshToken().getSecret().getBytes());
+    }
     @Override
     public String genAccessToken(AccessTokenPayload accessTokenPayload) throws Exception {
-         JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
-
-        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-            .subject(accessTokenPayload.getUserId().toString())
-            .expirationTime(
-                new Date(
-                    Instant.now()
-                        .plus(
-                            jwtProperties.getAccessToken().getExpiry(),
-                            ChronoUnit.HOURS
-                        )
-                        .toEpochMilli()
-                )
-            )
-            .claim("isShopOwnerActive", accessTokenPayload.getIsShopOwnerActive())
-            .build();
-
-        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
-        JWSObject jwsObject = new JWSObject(jwsHeader, payload);
-        jwsObject.sign(
-            new MACSigner(jwtProperties.getAccessToken().getSecret())
-        );
-        return jwsObject.serialize();
+        return Jwts.builder()
+                .setClaims(this.objectMapper.convertValue(accessTokenPayload, new TypeReference<Map<String, Object>>() {}))
+                .setSubject(accessTokenPayload.getUserId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getAccessToken().getExpiry()))
+                .signWith(this.accessTokenKey,SignatureAlgorithm.HS256)
+                .compact();
+                
     }
 
     @Override
     public String genRefreshToken(UUID userId) throws Exception {
-        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
-
-        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-            .subject(userId.toString())
-            .expirationTime(
-                new Date(
-                    Instant.now()
-                        .plus(
-                            jwtProperties.getRefreshToken().getExpiry(),
-                            ChronoUnit.HOURS
-                        )
-                        .toEpochMilli()
-                )
-            )
-            .build();
-
-        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
-        JWSObject jwsObject = new JWSObject(jwsHeader, payload);
-        jwsObject.sign(
-            new MACSigner(jwtProperties.getRefreshToken().getSecret())
-        );
-        return jwsObject.serialize();
+        return Jwts.builder()
+                .setClaims(Map.of("userId",userId))
+                .setSubject(userId.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshToken().getExpiry()))
+                .signWith(this.refreshTokenKey,SignatureAlgorithm.HS256)
+                .compact();
     }
 
    
